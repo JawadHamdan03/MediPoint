2fa920b21ba7b31d6a8c928bc949ead740a1f080
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-08-10 15:18:19 +0300

    remove result pattern

--- src/MediPoint.Domain/Common/Results/Error.cs
+++
@@ -1,3 +0,0 @@
-namespace MediPoint.Domain.Common.Results;
-
-public record Error(string Id, ErrorType Type, string Description);
--- src/MediPoint.Domain/Common/Results/Errors.cs
+++
@@ -1,6 +0,0 @@
-namespace MediPoint.Domain.Common.Results;
-
-public static class Errors
-{
-    public static Error NotFound { get; } = new("NotFound", ErrorType.NotFound, "Account not found.");
-}
