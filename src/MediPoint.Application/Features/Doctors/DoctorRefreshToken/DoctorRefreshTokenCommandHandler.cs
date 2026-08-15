9a6686b4adae526477d6877fd40b264d12c9b430
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-08-15 09:31:23 +0300

    fix load doctor with doctorRefreshToken

--- src/MediPoint.Application/Features/Doctors/DoctorRefreshToken/DoctorRefreshTokenCommandHandler.cs
+++ src/MediPoint.Application/Features/Doctors/DoctorRefreshToken/DoctorRefreshTokenCommandHandler.cs
@@ -2,7 +2,8 @@
 using MediPoint.Application.Common;
 using MediPoint.Application.Common.ServiceResponse;
 using MediPoint.Application.Common.Services;
-using MongoDB.Driver.Linq;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace MediPoint.Application.Features.Doctors.DoctorRefreshToken;
 
@@ -10,7 +11,9 @@ public class DoctorRefreshTokenCommandHandler(IAppDbContext dbContext,IJwtTokenS
 {
     public async Task<JwtTokenResponse> Handle(DoctorRefreshTokenCommand request, CancellationToken cancellationToken)
     {
-       var rf = await dbContext.DoctorRefreshTokens.FirstOrDefaultAsync(r=>r.Token.Equals(request.refreshToken));
+        var rf = await dbContext.DoctorRefreshTokens
+            .Include(r => r.Doctor)
+            .FirstOrDefaultAsync(r => r.Token == request.refreshToken);
        if (rf is null)
        {
            throw new Exception("No refresh token for this user, login first");
