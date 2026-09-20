8b81878ee6f9642ccf2db414627deb4f19f3642f
JawadHamdan03 <eng.jawad121@hotmail.com>
2026-09-20 21:30:37 +0300

    validation behavior test

--- tests/MediPoint.Application.UnitTests/Behaviors/ValidationBehaviorTest.cs
+++ tests/MediPoint.Application.UnitTests/Behaviors/ValidationBehaviorTest.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
 using MediPoint.Application.Common.Behaviors;
 using MediPoint.Application.Features.Admins.AdminAddsDoctor;
@@ -10,26 +11,93 @@ namespace MediPoint.Application.UnitTests.Behaviors;
 
 public class ValidationBehaviorTest
 {
-    private readonly ValidationBehavior<AddDoctorCommand, DoctorDto> _validationBehavior;
-    private readonly IEnumerable<IValidator<AddDoctorCommand>> _mockValidator;
+    private readonly AddDoctorCommand _command = new(new DoctorDto());
     private readonly RequestHandlerDelegate<DoctorDto> _mockNextBehavior;
+    private readonly DoctorDto _expectedResponse = new();
 
     public ValidationBehaviorTest()
     {
         _mockNextBehavior = Substitute.For<RequestHandlerDelegate<DoctorDto>>();
-        _mockValidator = Substitute.For<IEnumerable<IValidator<AddDoctorCommand>>>();
-        
-        _validationBehavior = new(_mockValidator);
+        _mockNextBehavior.Invoke(Arg.Any<CancellationToken>()).Returns(_expectedResponse);
     }
 
+    [Fact]
+    public async Task InvokeValidationBehavior_WhenNoValidatorsRegistered_ShouldInvokeNextBehavior()
+    {
+        // Arrange
+        var validators = new List<IValidator<AddDoctorCommand>>();
+        var validationBehavior = new ValidationBehavior<AddDoctorCommand, DoctorDto>(validators);
+
+        // Act
+        var result = await validationBehavior.Handle(_command, _mockNextBehavior, CancellationToken.None);
+
+        // Assert
+        Assert.Same(_expectedResponse, result);
+        await _mockNextBehavior.Received(1).Invoke(Arg.Any<CancellationToken>());
+    }
 
     [Fact]
     public async Task InvokeValidationBehavior_WhenValidatorResultIsValid_ShouldInvokeNextBehavior()
     {
-        // Arrange 
-        
-        // Act 
-        
+        // Arrange
+        var mockValidator = Substitute.For<IValidator<AddDoctorCommand>>();
+        mockValidator
+            .ValidateAsync(Arg.Any<ValidationContext<AddDoctorCommand>>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult());
+
+        var validationBehavior = new ValidationBehavior<AddDoctorCommand, DoctorDto>([mockValidator]);
+
+        // Act
+        var result = await validationBehavior.Handle(_command, _mockNextBehavior, CancellationToken.None);
+
         // Assert
+        Assert.Same(_expectedResponse, result);
+        await _mockNextBehavior.Received(1).Invoke(Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task InvokeValidationBehavior_WhenValidatorResultIsInvalid_ShouldThrowValidationExceptionAndNotInvokeNextBehavior()
+    {
+        // Arrange
+        var failure = new ValidationFailure("FirstName", "First name is required");
+        var mockValidator = Substitute.For<IValidator<AddDoctorCommand>>();
+        mockValidator
+            .ValidateAsync(Arg.Any<ValidationContext<AddDoctorCommand>>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult([failure]));
+
+        var validationBehavior = new ValidationBehavior<AddDoctorCommand, DoctorDto>([mockValidator]);
+
+        // Act
+        var act = () => validationBehavior.Handle(_command, _mockNextBehavior, CancellationToken.None);
+
+        // Assert
+        var exception = await Assert.ThrowsAsync<ValidationException>(act);
+        Assert.Contains(exception.Errors, e => e.PropertyName == "FirstName");
+        await _mockNextBehavior.DidNotReceive().Invoke(Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task InvokeValidationBehavior_WhenMultipleValidatorsAndOneFails_ShouldThrowValidationExceptionWithAggregatedErrors()
+    {
+        // Arrange
+        var passingValidator = Substitute.For<IValidator<AddDoctorCommand>>();
+        passingValidator
+            .ValidateAsync(Arg.Any<ValidationContext<AddDoctorCommand>>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult());
+
+        var failingValidator = Substitute.For<IValidator<AddDoctorCommand>>();
+        failingValidator
+            .ValidateAsync(Arg.Any<ValidationContext<AddDoctorCommand>>(), Arg.Any<CancellationToken>())
+            .Returns(new ValidationResult([new ValidationFailure("Email", "Email is invalid")]));
+
+        var validationBehavior = new ValidationBehavior<AddDoctorCommand, DoctorDto>([passingValidator, failingValidator]);
+
+        // Act
+        var act = () => validationBehavior.Handle(_command, _mockNextBehavior, CancellationToken.None);
+
+        // Assert
+        var exception = await Assert.ThrowsAsync<ValidationException>(act);
+        Assert.Single(exception.Errors);
+        await _mockNextBehavior.DidNotReceive().Invoke(Arg.Any<CancellationToken>());
     }
 }
